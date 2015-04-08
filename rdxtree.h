9ea638395afd04846d4accbc10b6bbdf6abcffb2
Richard Braun <rbraun@sceen.net>
2015-04-08 15:58:35 +0200

    rdxtree: update copyright notice

--- rdxtree.h
+++ rdxtree.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2011-2014 Richard Braun.
+ * Copyright (c) 2011-2015 Richard Braun.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
