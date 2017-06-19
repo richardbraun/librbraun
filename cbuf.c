1b21fa6b63b3f5583f05bc9dc6736afd11accb3b
Richard Braun <rbraun@sceen.net>
2017-06-19 21:13:47 +0200

    cbuf: update copyright year

--- cbuf.c
+++ cbuf.c
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Richard Braun.
+ * Copyright (c) 2015-2017 Richard Braun.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
