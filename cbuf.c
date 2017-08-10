f03d432d9d29bdcb07a4554c458617d243bb71a8
Richard Braun <rbraun@sceen.net>
2017-08-10 21:19:20 +0200

    Minor license fixes

--- cbuf.c
+++ cbuf.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2015-2017 Richard Braun.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
