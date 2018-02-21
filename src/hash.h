a484ccff7df6300378a0a62238fa6082879a3888
Richard Braun <rbraun@sceen.net>
2018-02-21 21:10:00 +0100

    hash: fix hash_str with large values of bits
    Reported by Laurent Dufresne.

--- src/hash.h
+++ src/hash.h
@@ -118,7 +118,7 @@ hash_str(const char *str, unsigned int bits)
         hash = ((hash << 5) - hash) + c;
     }
 
-    return hash & ((1 << bits) - 1);
+    return hash & ((1UL << bits) - 1);
 }
 
 #endif /* _HASH_H */
